1f3563650f8a9dd3418b32cb6284fc24d298d6d9
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2018-10-06 00:16:27 +0300

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-employee = User.create! email: 'employee@example.com', password: 'qweqwe'
+employee = User.create! email: 'employee@example.com', first_name: 'John', last_name: 'Doe', password: 'qweqwe'
