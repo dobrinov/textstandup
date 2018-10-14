f24def8664a9d1f32758727663fee8895653a794
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2018-10-14 21:24:27 +0300

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
-employee = User.create! email: 'employee@example.com', first_name: 'John', last_name: 'Doe', password: 'qweqwe'
-team = Team.create! name: 'Navy Seals'
+team = Team.create! name: 'The Simpsons'
 
-team.members << employee
-team.admins << employee
+homer  = User.create! email: 'homer@example.com',  first_name: 'Homer',  last_name: 'Simpsons', password: 'qweqwe'
+marge  = User.create! email: 'marge@example.com',  first_name: 'Marge',  last_name: 'Simpsons', password: 'qweqwe'
+bart   = User.create! email: 'bart@example.com',   first_name: 'Bart',   last_name: 'Simpsons', password: 'qweqwe'
+lisa   = User.create! email: 'lisa@example.com',   first_name: 'Lisa',   last_name: 'Simpsons', password: 'qweqwe'
+maggie = User.create! email: 'maggie@example.com', first_name: 'Maggie', last_name: 'Simpsons', password: 'qweqwe'
+
+team.members << homer
+team.members << marge
+team.members << bart
+team.members << lisa
+team.members << maggie
+
+team.admins << homer 
