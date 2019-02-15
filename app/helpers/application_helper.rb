1689e2d162d9b7f8f06ade1e2168ec70e87ec6cf
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2019-02-15 21:49:43 +0200

    Use the correct Slack scopes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def slack_authorization_url
-    "https://slack.com/oauth/authorize?scope=#{['users.profile:read', 'chat:write:bot'].join(' ')}&client_id=#{ENV['SLACK_CLIENT_ID']}"
+    "https://slack.com/oauth/authorize?scope=#{['users:read', 'chat:write:bot'].join(' ')}&client_id=#{ENV['SLACK_CLIENT_ID']}"
   end
 end
